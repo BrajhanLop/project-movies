6f1e7750ab48f65e247db297893405603b319219
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-05-19 19:49:10 -0500

    add routes

---
+++ src/app/Originales/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Originales = () => {
+  return (
+    <div>Originales</div>
+  )
+}
+
+export default Originales
--- src/app/about/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <div className=' text-red-700'>About</div>
-  )
-}
-
-export default About
